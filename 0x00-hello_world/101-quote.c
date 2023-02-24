ea1fd5a93fa523afe721f3719086d22edbf01b62
ADewale_Adejumo <arthoremma@gmail.com>
2023-02-24 04:06:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void) 
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }

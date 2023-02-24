d53247e4b9db6c352ae7d0536637b32f1d8d8a61
ADewale_Adejumo <arthoremma@gmail.com>
2023-02-24 02:47:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main(void )
-
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 61);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"  61);
 
 	return (1);
 }

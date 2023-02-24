55174652362243ca44490eb977dba16a4ed7d8bc
ADewale_Adejumo <arthoremma@gmail.com>
2023-02-24 05:21:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,8 @@
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" -\
-			    Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	const char c[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, c, sizeof(msg) - 1);
 
 	return (1);
 }

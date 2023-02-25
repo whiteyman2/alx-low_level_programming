4440a978b16079e4f76d0f8adf02c115c9f63d98
ADewale_Adejumo <arthoremma@gmail.com>
2023-02-24 17:24:49 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 	{
 		for (j = i; j < 100; j++)
 		{
-			putchar(i/10 + '0');
-			putchar(i%10 + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar(j/10 + '0');
-			putchar(j%10 + '0');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 		if (i < 99 || j < 99)
 		{
 			putchar(',');

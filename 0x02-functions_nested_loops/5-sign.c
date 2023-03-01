78e961d58d36a28a8e8a2eddb08e40e3c58de0c1
ADewale_Adejumo <arthoremma@gmail.com>
2023-03-01 15:15:46 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,20 +5,20 @@
  * @n: number input
  * Return: 1 prints '+' if n > o, 0 prints '0' if n = 0, -1 prints '-' if n < o
  */
-int print_sign(0)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{	_putchar('0');
-		return (1);
+	else if (n < 0)
+	{	_putchar('-')
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }

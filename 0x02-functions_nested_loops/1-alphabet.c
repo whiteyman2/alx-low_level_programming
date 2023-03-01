f224c6048bca1b22684ee854c2080d25a9ad6632
ADewale_Adejumo <arthoremma@gmail.com>
2023-03-01 00:58:37 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{	
+		_putchar(t);
+	}
+	-putchar('\n')
+
+	return (0);
+}

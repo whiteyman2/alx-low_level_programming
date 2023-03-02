a4148309b162e85beb25d76916bcc8f2f55689b4
ADewale_Adejumo <arthoremma@gmail.com>
2023-03-02 15:29:04 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,7 @@ void times_table(void)
 
 		if (product < 10)
 		{
+			_putchar(',');
 			_putchar(' ');
 		}
 		else
@@ -27,7 +28,6 @@ void times_table(void)
 		_putchar((product % 10) + '0');
 		_putchar(' ');
 	}
-	_putchar(',');
 	_putchar('\n');
 	}
 }

4bec3587e33bf5098dfaa5ca3432869e9ffbfff6
ADewale_Adejumo <arthoremma@gmail.com>
2023-03-02 18:00:59 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,43 +7,5 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
-
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-		int product = i * j;
-		if (product < 10)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(product + '0');
-		}
-		else if (product < 100)
-		{	
-			_putchar(' ');
-			_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
-		}
-		else
-		{
-			_putchar((product / 100) + '0');
-			_putchar(((product / 10) % 10) + '0');
-			_putchar((product % 10) + '0');
-		}
-
-		if (j != n)
-		{	
-			_putchar(',');
-			_putchar(' ');
-		}
-		}
-	
-		_putchar('\n');
-	}
+	return (0);
 }

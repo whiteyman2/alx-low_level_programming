d7809c823f4a609b39fcd396e481903075a69ac5
ADewale_Adejumo <arthoremma@gmail.com>
2023-03-02 16:22:19 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,16 +16,15 @@ void times_table(void)
 	{
 		int product = i * j;
 
-		if (product < 10)
+		if (product > 9)
 		{
-			_putchar(' ');
+			 _putchar((product % 10) + '0');
+			 _putchar((product / 10) + '0');
 		}
 		else
 		{
-			_putchar((product / 10) + '0');
+			_putchar((product0) + '0');
 		}
-		_putchar((product % 10) + '0');
-
 		if (j < 9)
 		{
 			_putchar(',');
